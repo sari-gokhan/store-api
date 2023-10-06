a9b99bc65e8690dadd45a9f46f4243492f01cccc
Gokhan Sari <gsari@Gokhans-MacBook-Pro.local>
2023-10-06 16:39:34 +0300

    Added routes and product schema

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('04 Store API')
+require('dotenv').config();
+require('express-async-errors')
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route </a>')
+});
+
+app.use('/api/v1/products', productsRouter);
+
+// Products route
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(PORT, console.log(`Server is listening port ${PORT}`))
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+start();
